534bfba5febdd5e176a499db41218b5682b03ce3
Adriano Kerber <kerberpro@gmail.com>
2019-08-07 17:52:26 -0300

    Add new step

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,5 +1,21 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
 module.exports = {
-    store(req, res) {
-        return res.json({ ok: true });
+    async store(req, res) {
+        const { username } = req.body;
+
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+
+        const { name, bio, avatar_url: avatar } = response.data;
+
+        const dev = await Dev.create({
+            name,
+            user: username,
+            bio,
+            avatar
+        });
+
+        return res.json(dev);
     }
 };
