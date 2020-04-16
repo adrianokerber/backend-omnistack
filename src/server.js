abdbb72e8834f7bc2c36f54f346ceacd1bd670f7
Adriano Kerber <kerberpro@gmail.com>
2020-04-16 10:37:21 -0300

    Add socket.io integration with http server and set basic usage example with frontend

--- src/server.js
+++ src/server.js
@@ -4,14 +4,30 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log('Nova conexão', socket.id);
+
+    socket.on('hello', message => {
+        console.log(message);
+    });
+
+    setTimeout(() => {
+        socket.emit('world', {
+            message: 'OmniStack'
+        });
+    }, 5000);
+});
 
 mongoose.connect('mongodb+srv://omniStack:y2uzXjC2nFLdHV9z@clusterzero-09qhx.mongodb.net/omnistack?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
